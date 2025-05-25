c439d4d5d253713b42ddd716f5e747052bcf1649
Bayoude <aymtaa@yahoo.com>
2025-05-25 15:47:07 +0200

    new message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//test
+﻿using System;
+
+Console.WriteLine("Quel est votre nom ?");
+        string? nom = Console.ReadLine();
+        Console.WriteLine($"Bonjour, {nom} !");
+
+        // Exemple de calcul
+        int a = 5, b = 3;
+        Console.WriteLine($"La somme de {a} et {b} est {a + b}");
